f02be671e8452995bdb835b8237a4d4f68ee3fac
serpient <littlegangwolf@gmail.com>
2019-01-31 17:14:23 -0800

    separate schemas; link them together using linkSchema

---
+++ src/schema/index.js
@@ -0,0 +1,33 @@
+import { gql } from 'apollo-server-express';
+
+import userSchema from './user';
+import messageSchema from './message';
+
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+
+  type Mutation {
+    _: Boolean
+  }
+
+  type Subscription {
+    _: Boolean
+  }
+`
+
+export default [linkSchema, userSchema, messageSchema];
+
+
+/*
+In this file, both schemas are merged with the help of a utility 
+called linkSchema. The linkSchema defines all types shared within
+the schemas. It already defines a Subscription type for GraphQL 
+subscriptions, which may be implemented later. As a workaround, 
+there is an empty underscore field with a Boolean type in the 
+merging utility schema, because there is no official way of 
+completing this action yet. The utility schema defines the shared
+base types, extended with the extend statement in the other 
+domain-specific schemas. 
+*/
