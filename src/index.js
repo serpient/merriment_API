509d5ddab4b8c797a33485ae69b8930e7aa765c8
serpient <littlegangwolf@gmail.com>
2019-01-31 16:56:19 -0800

    adding a createMessage mutation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors'
 import { ApolloServer, gql } from 'apollo-server-express';
 import 'dotenv/config';
+import uuidv4 from 'uuid/v4';
 
 const app = express(cors());
 
@@ -41,6 +42,10 @@ const schema = gql`
     message(id: ID!): Message!
   }
 
+  type Mutation {
+    createMessage(text: String!): Message!
+  }
+
   type User {
     id: ID!
     username: String!
@@ -52,6 +57,7 @@ const schema = gql`
     text: String!
     user: User!
   }
+  
 `
 const resolvers = {
   Query: {
@@ -70,6 +76,21 @@ const resolvers = {
     message: (parent, { id }) => {
       return messages[id];
     }
+  },
+
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message = {
+        id,
+        text,
+        userId: me.id,
+      }
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+      
+      return message;
+    },
   },
 
   User: {
