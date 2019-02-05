e96e9b2ab73ee4205044541404bfac384b2d6310
serpient <littlegangwolf@gmail.com>
2019-02-04 16:16:07 -0800

    add more details  to pagination

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -2,19 +2,23 @@ import { combineResolvers } from 'graphql-resolvers';
 import { isAuthenticated, isMessageOwner } from './authorization';
 import { Sequelize } from 'sequelize';
 
+const toCursorHash = string => Buffer.from(string).toString('base64');
+const fromCursorHash = string => Buffer.from(string, 'base64').toString('ascii');
+
 export default {
   Query: {
     messages: async (parent, { cursor, limit = 100 }, { models }) => {
       const cursorOptions = cursor
       ? {
           createdAt: {
-            [Sequelize.Op.lt]: cursor,
+            [Sequelize.Op.lt]: fromCursorHash(cursor),
           }
         }
       : null;
-      return await models.Message.findAll({
+
+      const messages = await models.Message.findAll({
         order: [['createdAt', 'DESC']],
-        limit,
+        limit: limit + 1,
         where: cursorOptions,
         /*
         the cursor is the createdAt property of a message. With Sequelize and other ORMs 
@@ -36,6 +40,25 @@ export default {
         next page of messages.
         */
       });  
+
+      const hasNextPage = messages.length > limit;
+      const edges = hasNextPage ? messages.slice(0, -1) : messages;
+      /*
+      You only retrieve one more message than defined in the limit. If the list of messages is 
+      longer than the limit, there is a next page; otherwise, there is no next page. You return 
+      the limited messages, or all messages if there is no next page. Now you can include the 
+      hasNextPage field in the pageInfo field. If you query messages with a limit of 2 and no 
+      cursor, you get true for the hasNextPage field. If query messages with a limit of more than 
+      2 and no cursor, the hasNextPage field becomes false. Then, your GraphQL client application 
+      knows that the list has reached its end.
+      */
+      return {
+        edges,
+        pageInfo: {
+          hasNextPage,
+          endCursor: toCursorHash(edges[edges.length - 1].createdAt.toString()),
+        },
+      }
     },
     message: async (parent, { id }, { models }) => {
       return await models.Message.findById(id);
