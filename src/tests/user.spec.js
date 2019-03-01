3f083ff4cb566dda003e377bbbc6e5da70a4716f
serpient <littlegangwolf@gmail.com>
2019-03-01 14:39:38 -0800

    add tests for signIn and deleteUser

--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -1,28 +1,27 @@
-import { expect } from 'chai';
-import * as userApi from './api';
+import { expect } from "chai";
+import * as userApi from "./api";
 
-describe('users', () => {
-  it('user is user', () => {
-    expect('user').to.eql('user');
+describe("users", () => {
+  it("user is user", () => {
+    expect("user").to.eql("user");
   });
 });
 
-describe('users', () => {
-  describe('user(id: String!): User', () => {
-    it ('returns a user when user can be found', async () => {
-
+describe("users", () => {
+  describe("user(id: String!): User", () => {
+    it("returns a user when user can be found", async () => {
       const expectedResult = {
         data: {
           user: {
-            id: '1',
-            username: 'rwieruch',
-            email: 'hello@robin.com',
-            role: 'ADMIN',
-          },
-        },
+            id: "1",
+            username: "rwieruch",
+            email: "hello@robin.com",
+            role: "ADMIN"
+          }
+        }
       };
 
-      const result = await userApi.user({ id: '1' });
+      const result = await userApi.user({ id: "1" });
 
       expect(result.data).to.eql(expectedResult);
       /*
@@ -32,6 +31,39 @@ describe('users', () => {
       works in between. A request goes through the whole GraphQL server stack 
       from API to database. 
       */
-    })
-  })
+    });
+
+    it("returns null when user cannot be found", async () => {
+      const expectedResult = {
+        data: {
+          user: null
+        }
+      };
+
+      const result = await userApi.user({ id: "42" });
+
+      expect(result.data).to.eql(expectedResult);
+    });
+  });
+
+  describe("deleteUser(id: String!): Boolean!", () => {
+    it("returns an error because only admins can delete a user", async () => {
+      const {
+        data: {
+          data: {
+            signIn: { token }
+          }
+        }
+      } = await userApi.signIn({
+        login: "ddavids",
+        password: "ddavids"
+      });
+      console.log(token);
+      const {
+        data: { errors }
+      } = await userApi.deleteUser({ id: "1" }, token);
+
+      expect(errors[0].message).to.eql("Not authorized as admin.");
+    });
+  });
 });
